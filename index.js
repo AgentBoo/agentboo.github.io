f076846dca834e550a0e7e5373668ed0969ae926
AgentBoo <zuzi.han@gmail.com>
2017-10-14 01:21:06 -0400

    added basics for @media rules and fixed misc styling

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// #js-email
+const envelope = document.getElementById("js-envelope");
+
+envelope.addEventListener("click", function(ev){
+	if (!envelope.classList.contains("css-email")){
+		envelope.classList.add("css-email")
+	} else {
+		envelope.classList.remove("css-email");
+	}
+})
+
 
 const navLi = document.querySelectorAll(".navigation li");
 
-navLi.forEach(function(li) {
-	li.addEventListener("click", function(ev) {
-  	navLi.forEach(function(link) {
+navLi.forEach(function(li){
+	li.addEventListener("click", function(ev){
+  	navLi.forEach(function(link){
     	link.classList.remove("active");
     });
     this.classList.add("active");
