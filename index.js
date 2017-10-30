6ffa2584cedd8a1dceb3bbee8454b0a18013b603
AgentBoo <zuzi.han@gmail.com>
2017-10-30 08:13:20 -0400

    switched content to master_backup

---
+++ index.js
@@ -0,0 +1,35 @@
+// NOTE: Animated envelope with email address
+const envelope = document.getElementById("js-envelope");
+
+envelope.addEventListener("click", function(ev){
+	if (!envelope.classList.contains("css-email")){
+		envelope.classList.add("css-email")
+	} else {
+		envelope.classList.remove("css-email");
+	}
+})
+
+// NOTE: Pretending I am serving multiple pages and making the nav bar 'reflect that'
+const navLi = document.querySelectorAll("#menu-nav li a");
+
+navLi.forEach(function(li){
+	li.addEventListener("click", function(ev){
+  	navLi.forEach(function(link){
+    	link.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+})
+
+// NOTE: Responsive menu bar
+const nav = document.getElementById('menu-nav');
+const bars = document.getElementById('menu-bars');
+
+bars.addEventListener('click', function(ev){
+	if (nav.className === "ul-nav"){
+		nav.classList.remove('ul-nav')
+		nav.classList.add('bars-nav');
+	} else {
+		nav.className = 'ul-nav'
+	}
+})
