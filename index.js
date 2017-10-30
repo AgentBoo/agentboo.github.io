4177910532768d609b877208487bbf55f8af8207
AgentBoo <zuzi.han@gmail.com>
2017-10-30 14:26:35 -0400

    all elements present in html document

--- index.js
+++ index.js
@@ -21,3 +21,21 @@ bars.addEventListener('click', function(ev){
 		nav.className = 'ul-nav'
 	}
 })
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
